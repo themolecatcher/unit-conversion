07aa1ddf0ff8f5cb629474004c220e9e2fa96525
Valesca <93053203+themolecatcher@users.noreply.github.com>
2023-11-11 07:50:54 +0100

    Update index.js
    changed let variables to const and made the innerHTML more readable by dividing the text over multiple lines

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const convertButton = document.getElementById("convert-button")
 const resetButton = document.getElementById("reset-button")
-let inputEl = document.getElementById("input-el")
-let lengthOutput = document.getElementById("length-output")
-let volumeOutput = document.getElementById("volume-output")
-let massOutput = document.getElementById("mass-output")
+const inputEl = document.getElementById("input-el")
+const lengthOutput = document.getElementById("length-output")
+const volumeOutput = document.getElementById("volume-output")
+const massOutput = document.getElementById("mass-output")
 
-/*the code below uses very long sentences, I feel like there has to be a way to make it shorter.
-I tried creating a variable "let feet = inputEl.value * 3.281" and add that in the template string but it didn't work.
-
-*/
 
 
 convertButton.addEventListener("click", function(){
-    lengthOutput.innerHTML = `${inputEl.value} meters = ${(inputEl.value * 3.281).toFixed(3)} feet 
-    | 
-    ${(inputEl.value * 3.281).toFixed(3)} feet =  ${inputEl.value} meters`
+    lengthOutput.innerHTML = `
+    ${inputEl.value} meters = ${(inputEl.value * 3.281).toFixed(3)} feet | 
+    ${(inputEl.value * 3.281).toFixed(3)} feet =  ${inputEl.value} meters
+    `
     
-    volumeOutput.innerHTML = `${inputEl.value} liters = ${(inputEl.value * 0.264).toFixed(3)} gallons 
-    | 
-    ${(inputEl.value * 0.264).toFixed(3)} gallons =  ${inputEl.value} liters`
+    volumeOutput.innerHTML = `
+    ${inputEl.value} liters = ${(inputEl.value * 0.264).toFixed(3)} gallons | 
+    ${(inputEl.value * 0.264).toFixed(3)} gallons =  ${inputEl.value} liters
+    `
     
-    massOutput.innerHTML = `${inputEl.value} kilos = ${(inputEl.value * 2.204).toFixed(3)} pounds | ${(inputEl.value * 2.204).toFixed(3)} pounds =  ${inputEl.value} kilos`
+    massOutput.innerHTML = `
+    ${inputEl.value} kilos = ${(inputEl.value * 2.204).toFixed(3)} pounds | 
+    ${(inputEl.value * 2.204).toFixed(3)} pounds =  ${inputEl.value} kilos
+    `
     
 })
 
